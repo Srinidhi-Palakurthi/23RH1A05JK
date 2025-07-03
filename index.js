610ec2800f22ebe6c0117f8d5c4ba33b1e20faef
Palakurthi Srinidhi <srinidhipalakurthi7@gmail.com>
2025-07-03 15:56:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-let valueName = true;
-console.log(valueName);
-let name="Srinidhi";
-console.log(name);
-console.log(typeof(valueName));
-var n=10;
-console.log(n);
-console.log(typeof(n));
-let key;
-let p=null;
-console.log(n);
-let Games = ["Chess", "Carrom", "Uno"];
-console.log(typeof(Games));
-console.log(Games);
-console.log(Games[0]);
-function animals(){
-    console.log("humans are animals");
+virtual2 = (a,b) => {
+    console.log("Addition is " + (a + b));
+    console.log("Subtraction is " + (a - b));
+    console.log("Multiplication is " + (a * b))
+    console.log("Division is " + (a / b));
 }
-//function call
-animals();
-
-let shoe = {
-    color:"white",
-    size:8,
-    brand:"Nike",
-    type:"Casual",
-    isWeight:false,
-    price:5000,
-    isLays:true,
-    gender:"female",
-    warrenty:2,
-    rating:4.5,
-}
-console.log(shoe);
-console.log(typeof(shoe));
-let num = 1;
-function outOfstock() {
-    //alert("Shoe is out of stock");
-    console.log(num);
-    num = num + 1;
-}
-let timeout = setTimeout
-(outOfstock, 3000);
-document.getElementary.getElementaryId('shoe')
-product = shoe;
-function submitFunction() {
-    console.log("Button clicked");
-}
+let x = 12;
+let y = 3;
+virtual2(x,y);
